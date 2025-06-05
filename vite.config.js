a7e4b4cb37a00142364c2de7709319d0a7629d2a
vanshikakashyap769@gmail.com <vanshikakashyap769@gmail.com>
2025-06-05 18:59:36 +0530

    third commmit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: `/Memory_Game/`
+  base: `/Memory_Game/`,
   plugins: [react(),tailwindcss(),],
 })
